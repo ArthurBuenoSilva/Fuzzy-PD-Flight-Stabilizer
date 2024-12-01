a313a0a4de6aeb1d307ab8cd0f74db75c6cc3f2c
Arthur Bueno Silva <arthurbueno1395171@hotmail.com>
2024-12-01 16:09:29 -0300

    :sparkles: New routes

--- app/fuzzy_pd/routes.py
+++ app/fuzzy_pd/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template
 
+from app.extensions import socketio
 from app.fuzzy_pd import bp
+from app.fuzzy_pd.controller import controller
 
 
 @bp.route("/")
 def index():
     return render_template("base.html")
+
+
+@socketio.on("go_to")
+def go_to(set_point):
+    controller.go_to(float(set_point))
+
+
+@socketio.on("rth")
+def rth():
+    controller.return_to_home()
