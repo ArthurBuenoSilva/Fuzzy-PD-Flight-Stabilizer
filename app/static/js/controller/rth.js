148fc36e1fcf4fd8b189f63a246d16b75c299a3b
Arthur Bueno Silva <arthurbueno1395171@hotmail.com>
2024-12-01 16:06:08 -0300

    :sparkles: Directional buttons and RTH functionality

--- app/static/js/controller/rth.js
+++ app/static/js/controller/rth.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const rth_button = document.getElementById("rth");
 
     rth_button.addEventListener("click", () => {
-
+        socketio.emit("rth")
     });
+
+    const go_to = document.getElementById('go_to');
+    go_to.addEventListener("click", function() {
+        const set_point = document.getElementById('set_point').value;
+        socketio.emit("go_to", set_point);
+    })
 })
